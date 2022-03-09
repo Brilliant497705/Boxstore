24f222931911769c29840597b31af3b730938e20
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-03-09 13:00:18 +0000

    final commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ class Navbar extends Component {
 
   render() {
     return (
-      <nav className="navbar navbar-dark bg-dark p-0 text-monospace">
+      <nav className="navbar navbar-dark bg-primary p-0 text-monospace">
         <a
           className="navbar-brand col-sm-3 col-md-2 mr-0"
           href="http://www.dappuniversity.com/bootcamp"
@@ -14,10 +14,31 @@ class Navbar extends Component {
           rel="noopener noreferrer"
         >
           <img src={box} width="30" height="30" className="align-top" alt="" />
-          D$t0r@g3
+          <small className='text-white'>Boxstore</small>
         </a>
         <ul className="navbar-nav px-3">
-          <b className='text-white'>{'0x0'}</b>
+        <li>
+        { this.props.account
+              ? <img
+                  alt=""
+                  className='mr-2'
+                  width='30'
+                  height='30'
+                  src={`data:image/png;base64,${new Identicon(this.props.account, 30).toString()}`}
+                />
+              : <span></span>
+            }
+            <small id="account">
+              <a target="_blank"
+                 alt=""
+                 className="text-white"
+                 rel="noopener noreferrer"
+                 href={"https://etherscan.io/address/" + this.props.account}>
+                {this.props.account ? this.props.account.substring(0,6) : '0x0'}...{this.props.account ? this.props.account.substring(38,42) : '0x0'}
+              </a>
+            </small>
+            
+          </li>
         </ul>
       </nav>
     );
